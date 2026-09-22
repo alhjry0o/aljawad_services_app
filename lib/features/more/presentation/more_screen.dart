814002f87209851cf77c2e203f6351ad3a9142d1
Abdullah AL-Hjry <abdullahyuosefalhjry@gmail.com>
2026-09-22 03:24:43 +0300

    Update more_screen.dart

--- lib/features/more/presentation/more_screen.dart
+++ lib/features/more/presentation/more_screen.dart
@@ -1,4 +1,4 @@
-import 'package:flutter/material';
+import 'package:flutter/material.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
 import '../../../core/theme/app_theme.dart';
 import '../../../core/constants/app_data.dart';
